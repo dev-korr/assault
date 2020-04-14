6bd249b8519a7112f413da9e7257ff9ab05a75a5
Tagore Korrapati <dev.korr2010@gmail.com>
2020-04-14 00:10:23 +0000

    Basic setup for porject.

--- setup.py
+++ setup.py
@@ -14,16 +14,14 @@ from setuptools import find_packages, setup, Command
 # Package meta-data.
 NAME = "mypackage"
 DESCRIPTION = "A python based web load testing tool."
-URL = "https://github.com/dev.korr/assault"
+URL = "https://github.com/dev-korr/assault.git"
 EMAIL = "dev.korr2010@gmail.com"
 AUTHOR = "Tagore Korrapati"
 REQUIRES_PYTHON = ">=3.7.0"
 VERSION = "0.1.0"
 
 # What packages are required for this module to be executed?
-REQUIRED = [
-    # 'requests', 'maya', 'records',
-]
+REQUIRED = ["click"]
 
 # What packages are optional?
 EXTRAS = {
@@ -106,9 +104,7 @@ setup(
     packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
     # If your package is a single module, use this instead of 'packages':
     # py_modules=['mypackage'],
-    # entry_points={
-    #     'console_scripts': ['mycli=mymodule:cli'],
-    # },
+    entry_points={"console_scripts": ["assault=assault.cli:cli"],},
     install_requires=REQUIRED,
     extras_require=EXTRAS,
     include_package_data=True,
